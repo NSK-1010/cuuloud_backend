091ab12d9337ae59eecc72285d94645669bb5f7e
TMP-tenpura <tmptenpura@gmail.com>
2021-09-04 06:03:52 +0900

    テーブルの定義の変更

--- model/user.py
+++ model/user.py
@@ -6,7 +6,8 @@ class User(db.Model):
     id = db.Column(db.String(length=255), primary_key=True, unique=True)
     password = db.Column(db.String(length=255))
     name = db.Column(db.Unicode(length=255))
-    email = db.Column(db.Unicode(length=255))
+    email = db.Column(db.Unicode(length=255), unique=True)
+    icon = db.Column(db.Unicode(length=255), unique=True)
     
     invitation_times_limit = db.Column(db.Integer)
     created_at = db.Column(db.DateTime(timezone=True), default=datetime.now)
